f7392a0d18d6df807e182b0252f075f129b45519
etrafa <84640450+etrafa@users.noreply.github.com>
2021-11-05 06:12:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+"using strict";
+
+const planetImage = document.querySelector(".planetImage");
+
+const planetImages = [
+  "./images/planet-earth.svg",
+  "./images/planet-mercury.svg",
+  "./images/planet-venus.svg",
+  "./images/planet-mars.svg",
+  "./images/planet-jupiter.svg",
+  "./images/planet-saturn.svg",
+  "./images/planet-uranus.svg",
+  "./images/planet-neptune.svg",
+];
